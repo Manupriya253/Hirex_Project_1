a7254c9c74718c631c9fe814675a8e91fb4963ca
Manupriya253 <subath687@gmail.com>
2025-05-08 13:16:35 +0530

    chore: Install React and initialize project folder structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TaskManagement from './pages/Dashboard/taskManagement';
+
+function App() {
+  return (
+    <div>
+    
+      <TaskManagement />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Dashboard/taskManagement.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TaskManagement = () => {
+  return (
+    <div>
+      <h1>Task Management Page</h1>
+      <p>This is a placeholder for the Task Management page. Content will be added soon.</p>
+    </div>
+  );
+};
+
+export default TaskManagement;
